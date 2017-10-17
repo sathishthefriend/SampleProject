59353e557b8c959b1010024187c872665d09de47
sathishthefriend <sathishthefriend@hotmail.com>
2017-10-16 23:50:43 -0500

    SampleProject

---
+++ src/com/hello/git/HelloGit.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.hello.git;
+
+/**
+ * @author Sathish
+ *
+ */
+public class HelloGit {
+
+	public void sayHelloGit() {
+		System.out.println("Hello Git!");
+	}
+}
---
+++ src/com/hello/world/HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.hello.world;
+
+import com.hello.git.HelloGit;
+
+/**
+ * @author Sathish
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		HelloGit helloGit = new HelloGit();
+		helloGit.sayHelloGit();
+	}
+
+}
